a74ef678440e5502f075bb7c6009601f56d4c181
PoprostuTaki <rafpiekar@gmail.com>
2019-11-19 23:26:09 +0100

    adding basic views, models, urls and templates for testing app

--- char_don_main/models.py
+++ char_don_main/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+TYPES = [
+    ('FUN', 'Fundacja'),
+    ('ORG', 'Organizacja pozarządowa'),
+    ('LOK', 'Zbiórka lokalna'),
+]
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.CharField(choices=TYPES, default='FU', max_length=3)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=16)
+    pick_up_datetime = models.DateTimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
+
--- char_don_main/views.py
+++ char_don_main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+from django.views.generic import TemplateView
+
+
+class LandingPageView(TemplateView):
+    template_name = 'index.html'
+
+
+class AddDonationView(TemplateView):
+    template_name = 'form.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+
+class RegisterView(TemplateView):
+    template_name = 'register.html'
